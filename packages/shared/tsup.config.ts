6b665d30c1f2be0eb7334855156f8b286a45798e
lengjing <853625225@qq.com>
2026-06-30 18:26:43 +0800

    update

---
+++ packages/shared/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: [
+    "src/index.ts",
+    "src/colors.ts",
+    "src/enums.ts",
+  ],
+  format: ["esm", "cjs"],
+  dts: true,
+  splitting: false,
+  clean: true,
+  outDir: "dist",
+});
